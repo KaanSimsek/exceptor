e6d34264ee55b3094cad9533901ff013eba4578d
Kaan <ksimsek19@ku.edu.tr>
2025-10-30 23:04:08 +0300

    remove feign client exceptions

--- src/main/java/com/exceptor/exceptor/autoconfigure/FeignExceptionControllerAdviceAutoConfiguration.java
+++ src/main/java/com/exceptor/exceptor/autoconfigure/FeignExceptionControllerAdviceAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.exceptor.exceptor.autoconfigure;
 
-import com.exceptor.exceptor.handler.FeignExceptionControllerAdvice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
